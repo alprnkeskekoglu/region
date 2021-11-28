e2c9e064a95f3aeb6d43d4745a6155262a133479
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2021-11-28 14:59:53 +0300

    initial commit

---
+++ src/RegionServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dawnstar\Region;
+
+use Illuminate\Support\ServiceProvider;
+
+class RegionServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
